b72f88edd7a7eb1cf56e3fb8c11a1d9da1374959
Randy Patterson <ranpat@microsoft.com>
2018-07-12 16:57:12 -0700

    Add project files.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient _client;
+        private IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _client = new HttpClient();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ViewResult> About()
+        {
+
+            var url = "http://" + _configuration["ApiHost"] + "/api/Hostname";
+            ViewData["HostName"] = await _client.GetStringAsync(url);
+            
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
