40b25a808c1a01c0245162ca001f3f70912f1b39
Randy Patterson <randy@randypatterson.com>
2019-06-20 12:15:35 -0500

    migrate to .NET Core 2.1

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -29,7 +29,6 @@ namespace WebApp
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
