a9b24785e1d842d23a79d65eea47b4ed6a8591cd
Randy Patterson <randy@randypatterson.com>
2019-06-20 12:50:28 -0500

    migrate to .NET Core 2.2

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp
 {
@@ -17,7 +18,7 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
